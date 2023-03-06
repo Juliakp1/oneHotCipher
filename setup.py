6c2ed13000ace25d3e1284890eee33e250cb07cc
juliapaiva1 <juliafpaiva@hotmail.com>
2023-03-06 09:47:40 -0300

    aps 2 arquivos biblioteca

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='oneHotCipher',
+    packages=find_packages(include=['oneHotCipher']),
+    version='0.1.0',
+    description='APS 2',
+    author='Júlia Karine e Júlia Paiva',
+    license='MIT',
+    install_requires=[],
+)
